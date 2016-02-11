3fc50671e0645e2608b2319670af60f82cd81399
Derfador1 <derfador1@gmail.com>
2016-02-11 15:38:47 -0500

    error with trying to set token inside of the while loop

--- intersect.c
+++ intersect.c
@@ -32,11 +32,21 @@ int main(int argc, char *argv[])
 			return 2;
 		}
 
-		while(!feof(fp)) {
-			char *buffer = malloc(256 * sizeof(*buffer)); //change to malloc later and realloc
-			fgets(buffer, 1, fp);
-			printf("Word: %s\n", buffer);
-		}		
+		if(file_count == 1) {
+			while(!feof(fp)) {
+				char *buffer = malloc(256 * sizeof(buffer)); //change to malloc later and realloc
+				if(fgets(buffer, 256, fp) != NULL) {
+					char *token;
+					while((token = strtok(buffer, '\0')) != NULL) {
+						printf("Inserting: %s", token);
+						hash_insert(hashy, token, file_count);
+					}
+				}
+			}
+		}	
+		else {
+			
+		}	
 
 		fclose(fp);
 		++file_count;
