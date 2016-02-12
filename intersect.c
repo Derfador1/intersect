5cdb1c43f0e6ee756e6bd84f5d6cee70fd2f8774
Derfador1 <derfador1@gmail.com>
2016-02-12 14:58:31 -0500

    errors with valgrind but still gives me correct output

--- intersect.c
+++ intersect.c
@@ -42,7 +42,8 @@ int main(int argc, char *argv[])
 		if(!fp) {
 			perror("The file could not be opened\n");
 			free(buffer);
-			exit(1);
+			//fclose(fp);
+			break;
 		}
 
 		while(!feof(fp)) {
@@ -135,7 +136,7 @@ void ll_print(struct h_llist *h, size_t file_count)
 {
 	while(h) {
 		if(h->value == file_count) {
-			printf("%zd %s\n", h->value, h->key);
+			printf("%zd %s\n", h->value, h->key); //change to fprintf(stdout)
 		}
 
 		h = h->next;
